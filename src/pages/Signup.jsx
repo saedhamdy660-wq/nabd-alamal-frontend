d1213ca394d160ebf54b59f7322b3c8783e31f01
saedhamdy660-wq <saedhamdy660@gmail.com>
2026-09-08 16:03:51 +0300

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import api from "../api";
+import { api } from "../api";
 
 function BackIcon() {
   return (
