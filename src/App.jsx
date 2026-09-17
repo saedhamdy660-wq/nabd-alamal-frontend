69548a75a5a3a5105e7afe958e53712c5401a0dc
saedhamdy660-wq <saedhamdy660@gmail.com>
2026-09-17 17:59:59 +0300

    Fix import path for VerifyIdentity component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Welcome from "./pages/Welcome.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import PhoneVerify from "./pages/PhoneVerify.jsx";
-import VerifyIdentity from "./pages/VerifyIdentity.jsx";
+import VerifyIdentity from "./pages/VerifyIdentity";
 import LocationPermission from "./pages/LocationPermission.jsx";
 
 import Home from "./pages/Home.jsx";
